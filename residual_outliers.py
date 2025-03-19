5f2169e9d2b396b38a006fddb965da5291974cd4
TomiTsuma <tommytsuma7@gmail.com>
2025-03-19 08:26:20 +0000

    Changed total nitrogen residuals

--- residual_outliers.py
+++ residual_outliers.py
@@ -63,13 +63,12 @@ def residual_outliers(chemicals, version):
             df.to_csv(f"/home/tom/DSML125/DSML87/outputFiles/PCC_Classes/{chem}.csv")
             spectra.loc[spectra.index.isin(df.loc[df['Difference'] <= df['residual_outlier_limit']].sample_code)].to_csv(f'/home/tom/DSML125/DSML87/outputFiles/PCC1/{chem}.csv')
         elif chem == 'total_nitrogen':
-            df['residual_outlier_limit'] = df[chem].apply(lambda x: 0.2 * x if x > 0.15 else None)
-            df.loc[df[chem] < 0.15, 'residual_outlier_limit'] = 0.03
             df['Difference'] = abs(df['Difference'])
-            df.loc[df['Difference'] <= df['residual_outlier_limit'],'PCC_Class'] = 1
-            df.loc[df['Difference'] > df['residual_outlier_limit'],'PCC_Class'] = 3
+            df.loc[df['Difference'] < 1,'PCC_Class'] = 1
+            df.loc[df['Difference'] >= 1,'PCC_Class'] = 3
             df.to_csv(f"/home/tom/DSML125/DSML87/outputFiles/PCC_Classes/{chem}.csv")
-            spectra.loc[spectra.index.isin(df.loc[df['Difference'] > df['residual_outlier_limit']].sample_code)].to_csv(f'/home/tom/DSML125/DSML87/outputFiles/PCC3/{chem}.csv')
+            spectra.loc[spectra.index.isin(df.loc[df['PCC_Class'] > 1].sample_code)].to_csv(f'/home/tom/DSML125/DSML87/outputFiles/PCC3/{chem}.csv')
+            spectra.loc[spectra.index.isin(df.loc[df['PCC_Class'] == 1].sample_code)].to_csv(f'/home/tom/DSML125/DSML87/outputFiles/PCC1/{chem}.csv')
         elif (chem in redbooth_outliers_dict.keys()):
             print(chem, "In Redbooth Outliers")
             lower = redbooth_outliers_dict[chem][0]
